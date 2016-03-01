f9d98e243517a1e86cf5dce419feb7893c29c0e8
gianlucamateo <gianluca.mateo@gmail.com>
2016-03-01 17:29:14 +0100

    Initial Commit
    Initial BB-8 prototype

---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadController : MonoBehaviour {
+
+	public GameObject body;
+	public Vector3 baseOffset, variableOffset;
+
+	// Use this for initialization
+	void Start () {
+		variableOffset = Vector3.zero;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (variableOffset.magnitude > 1)
+			variableOffset.Normalize ();
+		Vector3 totaloffset = baseOffset + variableOffset/3f;
+		totaloffset.Normalize ();
+		totaloffset *= 0.5f;
+		transform.position = body.transform.position + totaloffset;	
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float thrust;
+	public GameObject head;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+    //before physics
+    void FixedUpdate()
+    {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		Vector3 vOffset = head.GetComponent<HeadController> ().variableOffset;
+		vOffset += movement / 10;
+		rb.AddForce (vOffset * thrust);
+		if (movement.magnitude == 0)
+			vOffset *= 0.9f;
+		head.GetComponent<HeadController> ().variableOffset = vOffset;
+    }
+
+
+
+}
