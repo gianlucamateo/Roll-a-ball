4703ff0131dfaa3ecc830310f5c48fc3a10a7891
gianlucamateo <gianluca.mateo@gmail.com>
2016-03-06 00:59:27 +0100

    Simple navigation logic for bb-8

---
+++ Assets/Scripts/NavScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavScript : MonoBehaviour {
+
+
+
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = new Vector3(5,0,0); 
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,17 @@ public class PlayerController : MonoBehaviour {
 	public float thrust;
 	public GameObject head;
 	public Rigidbody rb;
-
+	public NavMeshAgent nav;
+	public Vector3[] navPoints;
+	public Vector3 controlOverride = Vector3.zero;
+	public bool stopping = false,slowing = false;
+	public bool navigating = false;
+	public int navIndex = 0;
+	public Vector3 vOffset, movement;
+	public int stopCount = 0;
+	public float distance;
+	public float forward;
+	public float currentSpeed,targetSpeed;
 
 
 	void Start ()
@@ -19,18 +29,102 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKey (KeyCode.Space)) {
 			UnityEngine.VR.InputTracking.Recenter ();
 		}
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
-		Vector3 vOffset = head.GetComponent<HeadController> ().variableOffset;
-		vOffset += movement / 10;
-		rb.AddForce (vOffset * thrust);
-		if (movement.magnitude == 0)
-			vOffset *= 0.9f;
-		head.GetComponent<HeadController> ().variableOffset = vOffset;
+		if (Input.GetKey (KeyCode.N)) {
+			this.navPoints = nav.path.corners;
+			stopping = true;
+			navigating = true;
+			navIndex = 0;
+		}
+		if (Input.GetKey (KeyCode.P)) {
+			stopping = true;
+		}
+		if (stopping) {
+			stop (true);
+		}
+		if (slowing) {
+			stop (false);
+		}
 
+		if (navigating && navPoints.Length>0 && !(stopping || slowing)) {
+			distance = (navPoints [navIndex] - transform.position).magnitude;
+			targetSpeed = distance;
+			currentSpeed = rb.velocity.magnitude;
+			//if (targetSpeed > 2 * currentSpeed) {
+				controlOverride = navPoints [navIndex] - transform.position;
+			//}
+			/*
+			if (currentSpeed > 2 * targetSpeed) {
+				//forward = Vector3.Dot (vOffset, rb.velocity) > 0 ? Vector3.Dot (vOffset, rb.velocity) : 0;
+				controlOverride -= rb.velocity*(currentSpeed-targetSpeed);
+			}*/
+			if (distance < 0.5) {
+				slowing = true;
+				//stopping = true;
+
+				if (navPoints.Length > navIndex + 1) {
+					navIndex++;
+				} else {
+					navigating = false;
+					stopping = true;
+				}
+			}
+		}		
+		if (controlOverride.magnitude > 1) {
+			controlOverride.Normalize ();
+		}
+		float moveHorizontal = controlOverride.x == 0 ? Input.GetAxis ("Horizontal") : controlOverride.x;
+		float moveVertical = controlOverride.z == 0 ? Input.GetAxis ("Vertical") : controlOverride.z;
+		movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		if (movement.magnitude > 0) {
+			rb.angularDrag = 0.05f;
+		}
+		vOffset = head.GetComponent<HeadController> ().variableOffset;
+		vOffset += movement / 10;
+		vOffset.x = Mathf.Round (100f * vOffset.x) / 100f;
+		vOffset.y = Mathf.Round (100f * vOffset.y) / 100f;
+		vOffset.z = Mathf.Round (100f * vOffset.z) / 100f;
+
+		if (movement.magnitude == 0) {
+			vOffset *= 0.9f;
+			if (vOffset.magnitude < 0.1f) {
+				vOffset = Vector3.zero;
+			}
+		} else {
+			rb.AddForce (vOffset * thrust);
+		}
+		head.GetComponent<HeadController> ().variableOffset = vOffset;
 		
     }
+
+	void stop(bool fullstop)
+	{		
+		Vector3 velocity = rb.velocity;
+		controlOverride = -velocity;
+		rb.velocity *= 0.9f;
+		if (fullstop) {
+			if (rb.velocity.magnitude < 0.1) {
+				stopCount++;
+				if (stopCount > 50) {
+					vOffset = Vector3.zero;
+					rb.velocity = Vector3.zero;
+					movement = Vector3.zero;
+					rb.angularVelocity = Vector3.zero;
+					controlOverride = Vector3.zero;
+					head.GetComponent<HeadController> ().variableOffset = Vector3.zero;
+					stopCount = 0;
+					stopping = false;
+					rb.angularDrag = float.MaxValue;
+
+				}
+
+			}
+		} else {
+			if (Mathf.Abs(Vector3.Dot(rb.velocity,Vector3.Cross(navPoints[navIndex]-transform.position,Vector3.up))) < 0.2f&&vOffset.magnitude<0.3f) { //.2 .3
+				slowing = false;
+			}
+		}
+			
+	}
 
 
 
