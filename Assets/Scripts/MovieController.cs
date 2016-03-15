9b4097ab173d7a5ee0a22ac193663e6ebf3dd4af
gianlucamateo <gianluca.mateo@gmail.com>
2016-03-15 18:28:25 +0100

    fixed some things, prepare for TV

---
+++ Assets/Scripts/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieController : MonoBehaviour {
+
+	public MovieTexture movie;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.M)) {
+
+			Renderer r = GetComponent<Renderer>();
+			movie = (MovieTexture)r.material.mainTexture;
+
+			if (movie.isPlaying) {
+				movie.Pause();
+			}
+			else {
+				movie.Play();
+			}
+		}
+	}
+}
