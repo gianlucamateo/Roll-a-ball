ab3173c95fa511e9f9d64c3e134cc186f18416eb
gianlucamateo <gianluca.mateo@gmail.com>
2016-05-15 20:02:31 +0200

    Added mode for Instructions

--- Assets/Scripts/BlindController.cs
+++ Assets/Scripts/BlindController.cs
@@ -12,7 +12,9 @@ public class BlindController : SwitchScript {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.J)) {
+			Switch ();
+		}	
 	}
 
 	public override void Switch() {
